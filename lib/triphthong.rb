c459636dcd4acc2c17bc0456fdc5d2a484e43084
Piotr Szotkowski <chastell@chastell.net>
2011-10-26 10:32:39 +0200

    up the Pan Tadeusz compliance to 50%

--- lib/triphthong.rb
+++ lib/triphthong.rb
@@ -3,7 +3,17 @@
 module Triphthong
 
   def self.count_syllables string
-    string.scan(/\bau|a|ą|e|é|ę|o|ó|Ó|u|y|i(?![aąeéęoóu])/i).size
+    string.split(/\P{L}/).map do |word|
+      case word
+      when 'Baublis'    then 2
+      when 'Pauzylipu'  then 4
+      when 'familiant'  then 4
+      when 'feudalny'   then 3
+      when 'paulinowie' then 4
+      when 'spolia'     then 3
+      else word.scan(/^au|a|à|ą|e(?!y)|é|ę|o|ó|Ó|u|y|i(?![aąeéęoóu])/i).size
+      end
+    end.inject :+
   end
 
 end
