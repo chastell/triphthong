063b22a9c8a18de6a55dc61edc918835fb9d2b10
Piotr Szotkowski <chastell@chastell.net>
2011-10-29 21:19:39 +0200

    handle all edge cases except ‘kaukaskich’

--- lib/triphthong.rb
+++ lib/triphthong.rb
@@ -4,15 +4,7 @@ module Triphthong
 
   def self.count_syllables string
     string.split(/\P{L}/).map do |word|
-      case word
-      when 'laufer'
-        2
-      when 'aplauzem', 'feudalny', 'kaukaskich', 'klientów', 'teutoński', 'Tyzenhauz'
-        3
-      when 'Akademii', 'kokieterii', 'paulinowie', 'Tyzenhauzem'
-        4
-      else word.scan(/^au|a|à|ą|æ|e(?!y)|é|ę|o|ó|Ó|u|y|i(?![aąeéęoóu])/i).size
-      end
+      word.scan(/^au|a(?!u[flz])|à|ą|æ|e(?!u[dt]|y)|é|ę|o|ó|Ó|u|y|i(?![aąioóu]|[eéę](?!nt))/i).size
     end.inject :+
   end
 
