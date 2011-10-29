2fda809a725739f6fa5441bacf77aa387c591164
Piotr Szotkowski <chastell@chastell.net>
2011-10-29 21:34:57 +0200

    Triphthong.count_syllables: don’t split into words

--- lib/triphthong.rb
+++ lib/triphthong.rb
@@ -3,9 +3,7 @@
 module Triphthong
 
   def self.count_syllables string
-    string.split(/\P{L}/).map do |word|
-      word.scan(/^au|a(?!u[flz])|à|ą|æ|e(?!u[dt]|y)|é|ę|o|ó|Ó|u|y|i(?![aąioóu]|[eéę](?!nt))/i).size
-    end.inject :+
+    string.scan(/\bau|a(?!u[flz])|à|ą|æ|e(?!u[dt]|y)|é|ę|o|ó|Ó|u|y|i(?![aąioóu]|[eéę](?!nt))/i).size
   end
 
 end
