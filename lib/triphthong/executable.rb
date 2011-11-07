e8dc281c0aa6271fcf14ec2e1802815083fe7cc8
Piotr Szotkowski <chastell@chastell.net>
2011-11-07 22:21:00 +0100

    Executable#run: limit by cæsura requirement

--- lib/triphthong/executable.rb
+++ lib/triphthong/executable.rb
@@ -1,8 +1,11 @@
+# encoding: UTF-8
+
 module Triphthong class Executable
   def initialize args
     @opts = Trollop.options args do
-      opt :datadir,        'Location of the data dir', default: 'data'
-      opt :syllable_count, 'Number of syllables',      type: Integer
+      opt :caesura,        'Require cæsura after the given syllable', type: Integer
+      opt :datadir,        'Location of the data dir',                default: 'data'
+      opt :syllable_count, 'Number of syllables',                     type: Integer
     end
     @args = args
   end
@@ -13,7 +16,9 @@ module Triphthong class Executable
       @args.each do |src|
         File.open "#{@opts[:datadir]}/#{File.basename src}", 'w' do |dest|
           File.read(src).extend(Text).sentences.each do |sentence|
-            next if @opts[:syllable_count_given] and sentence.extend(Verse).syllable_count != @opts[:syllable_count]
+            sentence.extend Verse
+            next if @opts[:caesura_given]        and not sentence.has_caesura_after? @opts[:caesura]
+            next if @opts[:syllable_count_given] and sentence.syllable_count != @opts[:syllable_count]
             dest.puts sentence
           end
         end
--- spec/triphthong/executable_spec.rb
+++ spec/triphthong/executable_spec.rb
@@ -16,6 +16,17 @@ module Triphthong describe Executable do
       end
     end
 
+    it 'plucks only sentences with cæsura after the given syllable' do
+      Dir.mktmpdir do |tempdir|
+        Executable.new(['prepare', '-d', tempdir, '-c', '3', 'spec/fixtures/pan-tadeusz.intro.txt']).run
+        File.read("#{tempdir}/pan-tadeusz.intro.txt").must_equal <<-END.gsub /^\s*/, ''
+          Ojczyzno moja!
+          ty jesteś jak zdrowie: Ile cię trzeba cenić, ten tylko się dowie, Kto cię stracił.
+          Dziś piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po tobie.
+        END
+      end
+    end
+
     it 'plucks only sentences of the given number of syllables' do
       Dir.mktmpdir do |tempdir|
         Executable.new(['prepare', '-d', tempdir, '-s', '5', 'spec/fixtures/pan-tadeusz.intro.txt']).run
