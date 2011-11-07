05541a89af6fccfdc476fb2ce52d367a42631f4a
Piotr Szotkowski <chastell@chastell.net>
2011-11-07 22:27:19 +0100

    Text#sentences spec: verify enumeratorness

--- spec/triphthong/text_spec.rb
+++ spec/triphthong/text_spec.rb
@@ -3,10 +3,17 @@
 require_relative '../spec_helper'
 
 module Triphthong describe Text do
+  before do
+    @text = File.read('spec/fixtures/pan-tadeusz.intro.txt').extend Text
+  end
+
   describe '#sentences' do
+    it 'returns an Enumerator' do
+      @text.sentences.must_be_instance_of Enumerator
+    end
+
     it 'yields consecutive stentences from the given text' do
-      text = File.read('spec/fixtures/pan-tadeusz.intro.txt').extend Text
-      text.sentences.to_a.must_equal [
+      @text.sentences.to_a.must_equal [
         'Litwo!',
         'Ojczyzno moja!',
         'ty jesteś jak zdrowie: Ile cię trzeba cenić, ten tylko się dowie, Kto cię stracił.',
