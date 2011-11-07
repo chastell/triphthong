bf978245615deea3bfb66f4abd2d1ebf83088fc2
Piotr Szotkowski <chastell@chastell.net>
2011-11-07 22:17:26 +0100

    Executable#run: limit by syllable count

--- lib/triphthong/executable.rb
+++ lib/triphthong/executable.rb
@@ -1,7 +1,8 @@
 module Triphthong class Executable
   def initialize args
     @opts = Trollop.options args do
-      opt :datadir, 'Location of the data dir', default: 'data'
+      opt :datadir,        'Location of the data dir', default: 'data'
+      opt :syllable_count, 'Number of syllables',      type: Integer
     end
     @args = args
   end
@@ -11,7 +12,10 @@ module Triphthong class Executable
     when 'prepare'
       @args.each do |src|
         File.open "#{@opts[:datadir]}/#{File.basename src}", 'w' do |dest|
-          File.read(src).extend(Text).sentences.each { |sentence| dest.puts sentence }
+          File.read(src).extend(Text).sentences.each do |sentence|
+            next if @opts[:syllable_count_given] and sentence.extend(Verse).syllable_count != @opts[:syllable_count]
+            dest.puts sentence
+          end
         end
       end
     end
