3fbf20033b62c5bdd5a4f6833677b5f47ba82eab
Piotr Szotkowski <chastell@chastell.net>
2011-11-07 22:23:34 +0100

    Executable: rename --syllable-count to --syllables

--- lib/triphthong/executable.rb
+++ lib/triphthong/executable.rb
@@ -3,9 +3,9 @@
 module Triphthong class Executable
   def initialize args
     @opts = Trollop.options args do
-      opt :caesura,        'Require cæsura after the given syllable', type: Integer
-      opt :datadir,        'Location of the data dir',                default: 'data'
-      opt :syllable_count, 'Number of syllables',                     type: Integer
+      opt :caesura,   'Require cæsura after the given syllable', type: Integer
+      opt :datadir,   'Location of the data dir',                default: 'data'
+      opt :syllables, 'Number of syllables',                     type: Integer
     end
     @args = args
   end
@@ -17,8 +17,8 @@ module Triphthong class Executable
         File.open "#{@opts[:datadir]}/#{File.basename src}", 'w' do |dest|
           File.read(src).extend(Text).sentences.each do |sentence|
             sentence.extend Verse
-            next if @opts[:caesura_given]        and not sentence.has_caesura_after? @opts[:caesura]
-            next if @opts[:syllable_count_given] and sentence.syllable_count != @opts[:syllable_count]
+            next if @opts[:caesura_given]   and not sentence.has_caesura_after? @opts[:caesura]
+            next if @opts[:syllables_given] and sentence.syllable_count != @opts[:syllables]
             dest.puts sentence
           end
         end
