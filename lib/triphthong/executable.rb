a57236e1c2d5b03466d9ca6eb4849d89fbbad991
Piotr Szotkowski <chastell@chastell.net>
2011-11-10 15:33:01 +0100

    Executable: default structure and clarifications

--- lib/triphthong/executable.rb
+++ lib/triphthong/executable.rb
@@ -3,14 +3,14 @@
 module Triphthong class Executable
   def initialize args = ARGV
     opts = Trollop.options args do
-      opt :datadir,   'Location of the data dir', default: 'data'
-      opt :structure, 'X+Y syllable structure',   multi: true, type: String
+      opt :database,  'Location of the database', default: 'db.yml'
+      opt :structure, 'X+Y syllable structure',   default: '7+6', multi: true
     end
 
     Trollop.die '--structure must be of the form m+n (where m and n are numbers)' unless opts[:structure].all? { |s| s =~ /^\d+\+\d+$/ }
 
     @structures = opts[:structure]
-    @datadir    = opts[:datadir]
+    @database   = opts[:database]
     @action     = args.shift
     @paths      = args
   end
@@ -18,7 +18,7 @@ module Triphthong class Executable
   def run
     case @action
     when 'build-db'
-      db = YAML::Store.new @datadir
+      db = YAML::Store.new @database
       db.transaction do
         @structures.each { |str, _| db[str] = Hash.new { |str, rhyme| str[rhyme] = [] } }
         @paths.each do |path|
