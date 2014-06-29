312d44cc2cd62226a5ef53bdbf0cbec0a4bbcff8
Piotr Szotkowski <chastell@chastell.net>
2014-06-30 01:13:16 +0200

    CLIRhymer spec: switch word array syntax

--- spec/triphthong/cli_rhymer_spec.rb
+++ spec/triphthong/cli_rhymer_spec.rb
@@ -5,7 +5,7 @@ module Triphthong describe CLIRhymer do
     it 'parses the arguments and passes them to the rhymer' do
       rhymer = fake :rhymer, as: :class
       capture_io do
-        CLIRhymer.new(%w[--lenghts 7+6 --rhymes AABB], rhymer: rhymer).run
+        CLIRhymer.new(%w(--lenghts 7+6 --rhymes AABB), rhymer: rhymer).run
       end
       rhymer.must_have_received :rhyme, [{ lenghts: '7+6', rhymes: 'AABB' }]
     end
@@ -14,7 +14,7 @@ module Triphthong describe CLIRhymer do
       rhymer = fake :rhymer, as: :class
       stub(rhymer).rhyme(lenghts: '5-7-5', rhymes: 'ABC') { "poem\n" }
       lambda do
-        CLIRhymer.new(%w[--lenghts 5-7-5 --rhymes ABC], rhymer: rhymer).run
+        CLIRhymer.new(%w(--lenghts 5-7-5 --rhymes ABC), rhymer: rhymer).run
       end.must_output "poem\n"
     end
   end
