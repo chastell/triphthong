44ca0adafed64edda816fb66e9c6a0098e8c78cc
Piotr Szotkowski <chastell@chastell.net>
2014-07-16 05:36:54 +0200

    spec Settings#rhymes

--- spec/triphthong/settings_spec.rb
+++ spec/triphthong/settings_spec.rb
@@ -8,4 +8,11 @@ module Triphthong describe Settings do
       Settings.new(%w(--lenghts 7+6)).lenghts.must_equal '7+6'
     end
   end
+
+  describe '#rhymes' do
+    it 'returns the parsed -r or --rhymes value' do
+      Settings.new(%w(-r AABB)).rhymes.must_equal       'AABB'
+      Settings.new(%w(--rhymes AABB)).rhymes.must_equal 'AABB'
+    end
+  end
 end end
