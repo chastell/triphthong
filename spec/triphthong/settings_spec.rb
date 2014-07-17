9e82f25b612612057d3b0d6d21c54a387a143706
Piotr Szotkowski <chastell@chastell.net>
2014-07-18 00:13:29 +0200

    Settings spec: oh how much more exciting

--- spec/triphthong/settings_spec.rb
+++ spec/triphthong/settings_spec.rb
@@ -4,15 +4,15 @@ require_relative '../../lib/triphthong/settings'
 module Triphthong describe Settings do
   describe '#lenghts' do
     it 'returns the parsed -l or --lenghts value' do
-      Settings.new(%w(-l 7+6)).lenghts.must_equal        '7+6'
-      Settings.new(%w(--lenghts 7+6)).lenghts.must_equal '7+6'
+      Settings.new(%w(-l 7+6)).lenghts.must_equal '7+6'
+      Settings.new(%w(--lenghts 5-7-5)).lenghts.must_equal '5-7-5'
     end
   end
 
   describe '#rhymes' do
     it 'returns the parsed -r or --rhymes value' do
-      Settings.new(%w(-r AABB)).rhymes.must_equal       'AABB'
-      Settings.new(%w(--rhymes AABB)).rhymes.must_equal 'AABB'
+      Settings.new(%w(-r AABB)).rhymes.must_equal 'AABB'
+      Settings.new(%w(--rhymes ABC)).rhymes.must_equal 'ABC'
     end
   end
 end end
