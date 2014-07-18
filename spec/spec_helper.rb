27df393cab9597f777d94fc305edf02bf95a57c7
Piotr Szotkowski <chastell@chastell.net>
2014-07-18 21:03:05 +0200

    spec_helper: add String#dedent

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ require 'bogus/minitest/spec'
 require 'triphthong'
 
 Bogus.configure { |config| config.search_modules << Triphthong }
+
+class String
+  def dedent
+    gsub(/^#{self[/\A\s*/]}/, '')
+  end
+end
