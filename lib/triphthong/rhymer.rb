1965c29a8d7d755538cf17e8c0eb8ed47270afe4
Piotr Szotkowski <chastell@chastell.net>
2014-08-17 18:29:13 +0200

    factor out Rhymer#{rhyme_{lengths,map},structure}

--- lib/triphthong/rhymer.rb
+++ lib/triphthong/rhymer.rb
@@ -13,11 +13,6 @@ module Triphthong
     end
 
     def poem
-      structure     = rhymes.zip lengths
-      rhyme_lengths = structure.group_by(&:first).map do |symbol, structs|
-        [symbol, structs.map(&:last)]
-      end.to_h
-      rhyme_map = verses.rhyme_map rhyme_lengths
       structure.map do |symbol, length|
         verses.verse length: length, rhyme: rhyme_map[symbol]
       end.join "\n"
@@ -25,5 +20,19 @@ module Triphthong
 
     attr_reader :lengths, :rhymes, :verses
     private     :lengths, :rhymes, :verses
+
+    def rhyme_lengths
+      structure.group_by(&:first).map do |symbol, structs|
+        [symbol, structs.map(&:last)]
+      end.to_h
+    end
+
+    def rhyme_map
+      @rhyme_map ||= verses.rhyme_map rhyme_lengths
+    end
+
+    def structure
+      @structure ||= rhymes.zip lengths
+    end
   end
 end
