7c182ed338d7fba7684f1cabd99d342847d9eb52
Piotr Szotkowski <chastell@chastell.net>
2015-05-17 08:07:45 +0200

    port Settings test to Expectation syntax

--- test/triphthong/settings_test.rb
+++ test/triphthong/settings_test.rb
@@ -5,14 +5,14 @@ module Triphthong
   describe Settings do
     describe '#lengths' do
       it 'returns the parsed -l or --lengths value' do
-        Settings.new(%w(--lengths 5-7-5)).lengths.must_equal [5, 7, 5]
+        _(Settings.new(%w(--lengths 5-7-5)).lengths).must_equal [5, 7, 5]
       end
     end
 
     describe '#rhymes' do
       it 'returns the parsed -r or --rhymes value' do
-        Settings.new(%w(-r AABB)).rhymes.must_equal %w(A A B B)
-        Settings.new(%w(--rhymes ABC)).rhymes.must_equal %w(A B C)
+        _(Settings.new(%w(-r AABB)).rhymes).must_equal %w(A A B B)
+        _(Settings.new(%w(--rhymes ABC)).rhymes).must_equal %w(A B C)
       end
     end
   end
